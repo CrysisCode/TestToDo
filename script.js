0231352bad9f9400df9405f20416307c7742913f
Tarik Topic <info@ttdigitalis.com>
2022-05-04 14:59:46 +0200

    The JavaScript To Do list  is done!

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
-function getRandomId(max){
-    return Math.floor(Math.random() * max);
-}
-
-var randomId = getRandomId(100);
+var randomId;
 
 function createTask() {
     let input = document.querySelector('.form-control');
-    console.log(input.value);
     
-    let newElement = document.createElement('li');
-    newElement.setAttribute("id", randomId);
-    newElement.classList = 'list-group-item d-flex justify-content-between align-items-center'; //ovako se kreira novi bootstrap element
-    newElement.innerText = input.value;
 
-    let btnClose = document.createElement('button');
-    btnClose.setAttribute("onclick", "deleteTask()");
-    btnClose.classList = 'btn text-light btn-close';
+    if(input.value === ""){
+        alert("You must write something!!!");
+    }
+    else {
+        console.log(input.value);
+        function getRandomId(max){
+            return Math.floor(Math.random() * max);
+        }
     
-    let newTask = document.querySelector('.list-group'); //u varijablu smo spremili gdje zelimo dodavati novi element
-    newElement.innerHTML += btnClose.outerHTML; //oznacili smo da zelimo dodijeliti ovom elementu koji je unutrasnji dodati jos jedan html element koji oznacavamo outerHTML
-    newTask.appendChild(newElement); //i ovdje pozivamo varijablu gdje zelimo spremati element i u njoj unutrasnji element koji ce dodati i outer unutar inner
+        randomId = getRandomId(100);
+    
+        let newElement = document.createElement('li');
+        newElement.setAttribute("id", randomId);
+        newElement.classList = 'list-group-item d-flex justify-content-between align-items-center'; //ovako se kreira novi bootstrap element
+        newElement.innerText = input.value;
+    
+        let btnClose = document.createElement('button');
+        btnClose.setAttribute("onclick", "this.parentNode.parentNode.removeChild(this.parentNode);");
+        btnClose.classList = 'btn text-light btn-close';
+        
+        let newTask = document.querySelector('.list-group'); //u varijablu smo spremili gdje zelimo dodavati novi element
+        newElement.innerHTML += btnClose.outerHTML; //oznacili smo da zelimo dodijeliti ovom elementu koji je unutrasnji dodati jos jedan html element koji oznacavamo outerHTML
+        newTask.appendChild(newElement); //i ovdje pozivamo varijablu gdje zelimo spremati element i u njoj unutrasnji element koji ce dodati i outer unutar inner
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
 
-for(let i = 0; i < 3;i++){
+for(let i = 0; i < 10; i++){
+
+    let randomId = 1;
+    randomId += i
+
+    console.log(randomId);
 
     let newElement = document.createElement('li');
     newElement.setAttribute("id", randomId);
     newElement.classList = 'list-group-item d-flex justify-content-between align-items-center'; //ovako se kreira novi bootstrap element
-    newElement.innerText = "input.value";
+    newElement.innerText = `Task ${randomId}`;
 
     let btnClose = document.createElement('button');
-    btnClose.setAttribute("onclick", "deleteTask()");
+    btnClose.setAttribute("onclick", "this.parentNode.parentNode.removeChild(this.parentNode);");
     btnClose.classList = 'btn text-light btn-close';
     
     let newTask = document.querySelector('.list-group'); //u varijablu smo spremili gdje zelimo dodavati novi element
     newElement.innerHTML += btnClose.outerHTML; //oznacili smo da zelimo dodijeliti ovom elementu koji je unutrasnji dodati jos jedan html element koji oznacavamo outerHTML
     newTask.appendChild(newElement); //i ovdje pozivamo varijablu gdje zelimo spremati element i u njoj unutrasnji element koji ce dodati i outer unutar inner
     }
-    var element = document.querySelectorAll('.btn-close');
-
-    for(var i = 0; i < element.length; i++){
-        element[i].addEventListener("click", function(){
-            let fixedTask = document.querySelector('.list-group-item');
-            fixedTask.remove();
-        })
-    }
 });
 
-function deleteTask() {
-    let task = document.getElementById(randomId);
+var taskRemove = document.querySelector('.btn-close')
+taskRemove.addEventListener("click", function(){
+    let task = document.querySelector(randomId);
     task.remove();
-}
+})
